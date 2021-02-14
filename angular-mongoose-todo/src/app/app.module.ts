f8f885275c01e22f63e4794cf6e25a97e90de335
olimorri <olivercharlesmorrison@gmail.com>
2021-02-14 20:10:00 +0000

    feat: updating imports to include new component

--- angular-mongoose-todo/src/app/app.module.ts
+++ angular-mongoose-todo/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { TodoInputComponent } from './todo-input/todo-input.component';
+import { HttpClientModule } from '@angular/common/http';  
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoInputComponent
   ],
   imports: [
     BrowserModule,
