daf43e391cf30c7baedece0c1f1498640c7813f0
olimorri <olivercharlesmorrison@gmail.com>
2021-02-14 15:46:41 +0000

    feat: updated router methods to include imported methods form the controller

--- server/router.js
+++ server/router.js
@@ -1,4 +1,6 @@
 const Router = require('koa-router');
 const router = new Router();
+const { getTodos, postTodo } = require('./controllers/todoController');
 
-router.get('/todo')
+router.get('/todo', getTodos);
+router.post('/todo', postTodo);
