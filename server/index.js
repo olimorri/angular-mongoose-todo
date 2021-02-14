f997dfc981f4a62d56ed12feb7bd0e6be92ad5fc
olimorri <olivercharlesmorrison@gmail.com>
2021-02-14 15:55:44 +0000

    feat: imported router and ensured app was using it - this enables basic get and post requests to the db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const Koa = require('koa');
 const app = new Koa();
-//TODO: add router - const router = require('./router')
+const router = require('./router')
 const bodyParser = require('koa-bodyparser');
 const mongoose = require('mongoose');
 const cors = require('@koa/cors');
@@ -8,7 +8,7 @@ const cors = require('@koa/cors');
 const port = 3000;
 app.use(cors());
 app.use(bodyParser());
-//TODO: enable this once router is sorted app.use(router.routes());
+app.use(router.routes());
 
 app.listen(port, () => {
   mongoose.connect(`mongodb://localhost:27017/angular-mongoose-todo`,{useNewUrlParser: true, useUnifiedTopology: true})
